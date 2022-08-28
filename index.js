2755fffb71dcb3c5c75de37b4e34e7e3ad05fe24
isir10 <isir.malak@moringaschool.com>
2022-08-28 16:01:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    return drivers
+      .filter(function (driver) {
+        return driver.toUpperCase().indexOf(names.toUpperCase()) !== -1;
+      })
+      .sort();
+  }
+
+function fuzzyMatch(drivers, names) {
+    return drivers
+      .filter(function (driver) {
+        return driver.toUpperCase().indexOf(names.toUpperCase()) === 0;
+      })
+      .sort();
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+      return driver.name.toUpperCase() === name.toUpperCase();
+    });
+  }
